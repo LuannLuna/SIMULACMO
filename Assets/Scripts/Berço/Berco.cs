5598b7fdf1befd2833075e739dcd5a11faf93b5f
LuannLuna <luann.marques@gmail.com>
2015-03-11 12:08:54 -0300

    Novo Design

---
+++ Assets/Scripts/Berço/Berco.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Berco : MonoBehaviour {
+	public bool empty;
+	NavioMoviment navioAtracado;
+	Controller _controll;
+
+	private Animator _anim;
+	// Use this for initialization
+	void Awake ()
+	{
+		empty = true; 
+		_anim = gameObject.GetComponent <Animator> ();
+		_controll = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent <Controller> ();
+	}
+
+	private void OnTriggerEnter2D (Collider2D coll)
+	{
+		if (coll.gameObject.tag == "Barco" && empty) {
+			navioAtracado = coll.gameObject.GetComponent <NavioMoviment> ();
+			navioAtracado._berco = _anim;
+			navioAtracado.destroyItSelf = true;
+			coll.gameObject.GetComponent <NavioMoviment> ().followMousePos = false;
+			empty = false;
+			Destroy (coll.gameObject.GetComponent <PolygonCollider2D> ());
+		} else {
+			coll.gameObject.GetComponent <NavioMoviment> ().backOriginalPos = true;
+		}
+		Debug.Log (coll.gameObject.name);
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Controller/Controller.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Controller : MonoBehaviour {
+	public GameObject navioModelo;
+	public Transform pointToRespawn;
+	public Transform mar;
+	public double timeToRespawn;
+	public bool canRespawn;
+	public int idBarcoSelecionado;
+
+	public Dictionary <int, GameObject> barcos = new Dictionary<int, GameObject> ();
+
+	private double timer;
+	private int idBarco;
+	private int i;
+	
+	public void Respawn ()
+	{
+		if (Time.time > timer)
+		{
+			timer = Time.time + timeToRespawn;
+			GameObject barco = (GameObject) Instantiate (navioModelo, pointToRespawn.position, pointToRespawn.rotation);
+			barco.GetComponent <NavioMoviment> ().id = idBarco;
+			barco.transform.parent = mar;
+			barcos.Add (idBarco, barco);
+			idBarco ++;
+			bool cargaDescarga = (Random.Range (0, 100) % 5 == 0);
+			barco.GetComponent <NavioMoviment> ().cargaDescarga = cargaDescarga;
+		}
+	}
+
+	public void StarAll (){
+		foreach (var barco in barcos) {
+			barco.Value.GetComponent <NavioMoviment> ().canMove = true;
+		}
+	}
+
+	public void StopAll (){
+		foreach (var barco in barcos) {
+			barco.Value.GetComponent <NavioMoviment> ().canMove = false;
+		}
+	}
+
+	private void PreventOverFlow (){
+		Vector2 pos = pointToRespawn.position;
+		RaycastHit2D hit = Physics2D.Raycast (pos, -Vector2.right, 2f);
+		Debug.DrawRay (pos, new Vector2 (-2f, 0f), Color.red);
+		if (hit.collider == null) {
+			canRespawn = true;
+		} else {
+			canRespawn = false;
+		}
+	}
+
+	public void RestarFila (){
+		StartCoroutine (StarBoat());
+	}
+
+
+	private IEnumerator StarBoat (){
+		yield return new WaitForSeconds (1.5f);
+		if (barcos[i] != null && i < barcos.Count){
+			barcos[i].GetComponent <NavioMoviment> ().canMove = true;
+			i++;
+			StarBoat ();
+		}else if (i >= barcos.Count){
+			i = 0;
+		}
+	}
+
+	private void Awake () {
+		canRespawn = true;
+		idBarcoSelecionado = -1;
+		idBarco = 0;
+	}
+
+	// Update is called once per frame
+	private void Update () {
+		if (canRespawn)
+		{
+			Respawn();
+		}
+		PreventOverFlow ();
+	}
+}
